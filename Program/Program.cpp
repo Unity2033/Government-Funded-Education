42c399d5c90cf410172afc362c508134bca09863
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-03-17 19:43:35 +0900

    (2025.3.17) 거품 정렬

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,17 +1,9 @@
 ﻿#include <iostream>
 
-#define SIZE 5
-
 using namespace std;
 
 int main()
 {
-#pragma region 삽입 정렬
-    // 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
-    // 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다.
-
-#pragma endregion
-
 
     return 0;
 }
