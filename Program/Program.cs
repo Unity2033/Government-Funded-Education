6dfa08b54c283d393321980feb2259aecc4d0836
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-08-25 15:42:32 +0900

    (2025.8.25) 매개 변수 한정자

---
+++ Program/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 박싱 
+            // 값 형식의 데이터를 참조 형식으로 변환하는 과정입니다.
+
+            // int value = 100;
+            // 
+            // object generic = value;
+            // 
+            // Console.WriteLine("value : " + value);
+            // Console.WriteLine("generic : " + generic);
+            #endregion
+
+            #region 언박싱
+            // 참조 형식의 데이터를 값 형식으로 변환하는 과정입니다.
+
+            // int box = (int)generic;
+            // 
+            // Console.WriteLine("box : " + box);
+            #endregion
+
+            Utility utility = new Utility() ;
+
+            utility.Pause();
+
+            int next = 10;
+            int previous = 0;
+
+            int distance = -4;
+
+            utility.Swap(ref next, ref previous);
+
+            Console.WriteLine("next 변수의 값 : " + next);
+            Console.WriteLine("previous 변수의 값 : " + previous);
+            Console.WriteLine("Absolute의 값 : " + utility.Absolute(in distance));
+        }
+    }
+}
---
+++ Program/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Utility
+    {
+        public void Pause()
+        {
+            Console.WriteLine("Pause....");
+        }
+
+        public void Swap(ref int x, ref int y)
+        {
+            int temporary = x;
+
+            x = y;
+
+            y = temporary;
+        }
+
+        public int Absolute(in int value)
+        {
+            return value < 0 ? -value : value; 
+        }
+    }
+}
