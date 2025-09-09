4bf53f5e2c090a75011d7a8667670e647a56ff8c
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-09-09 15:46:02 +0900

    (2025.9.9) 유저 인터페이스

--- Game/Assets/Class4th (Transform)/Scripts/Character.cs
+++ Game/Assets/Class4th (Transform)/Scripts/Character.cs
@@ -1,25 +1,17 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Move))]
 public class Character : MonoBehaviour
 {
-    [SerializeField] Vector3 direction;
-    [SerializeField] float speed = 10.0f;
+    [SerializeField] Move move;
 
-    void Start()
+    private void Awake()
     {
-     
+        move = GetComponent<Move>();
     }
 
     void Update()
-    {
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.z = Input.GetAxisRaw("Vertical");
-
-        // 벡터의 크기를 1로 설정합니다.
-        direction.Normalize();
-
-        // Time.deltaTime 
-        // 마지막 프레임 이후의 완료되는 시간
-        transform.position = transform.position + direction * speed * Time.deltaTime;
+    {    
+        move.OnMove(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")));
     }
 }
