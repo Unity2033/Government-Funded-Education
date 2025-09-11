4a95b5731969a60970c93decfdf9bf2afba3d196
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-09-11 15:28:26 +0900

    (2025.9.11) 프리팹 시스템

--- Game/Assets/Class5th (User Interface)/Scripts/Selectable.cs
+++ Game/Assets/Class5th (User Interface)/Scripts/Selectable.cs
@@ -1,7 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Selectable : MonoBehaviour
 {
-   
+    [SerializeField] Text titleText;
+
+    private void Awake()
+    {
+        titleText = GetComponentInChildren<Text>();
+    }
+
+    public void Enter()
+    {
+        titleText.fontSize = 100;
+    }
+
+    public void Exit()
+    {
+        titleText.fontSize = 75;
+    }
+
+    public void Down()
+    {
+        titleText.fontSize = 50;
+    }
 
 }
